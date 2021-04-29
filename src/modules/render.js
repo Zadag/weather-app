3aeaef997a1d69f45095d0b4e52b0358e051da42
Kevin <kbrowne97@gmail.com>
2021-04-29 18:36:47 -0400

    render changes

--- src/modules/render.js
+++ src/modules/render.js
@@ -14,15 +14,34 @@ const renderLogo = () => {
 const renderDaily = (daily) => {
   console.log(daily);
   const date = new Date();
-  const today = date.getDay();
+  let today = date.getDay();
+  const daysArr = [];
+  daysArr.push(today);
+  while (daysArr.length < 8) {
+    if (today === 6) {
+      today = 0;
+    } else {
+      today += 1;
+    }
+    daysArr.push(today);
+  }
+  console.log(daysArr);
 
-  daily.forEach((day) => {
+  for (let i = 0; i < 8; i += 1) {
     const card = document.createElement('div');
     card.classList.add('daily-card');
 
     const weekday = document.createElement('p');
     weekday.classList.add('weekday');
-    weekday.textContent = 'Monday';
+    let day = '';
+    if (daysArr[i] === 0) day = 'Sunday';
+    if (daysArr[i] === 1) day = 'Monday';
+    if (daysArr[i] === 2) day = 'Tuesday';
+    if (daysArr[i] === 3) day = 'Wedensday';
+    if (daysArr[i] === 4) day = 'Thursday';
+    if (daysArr[i] === 5) day = 'Friday';
+    if (daysArr[i] === 6) day = 'Saturday';
+    weekday.textContent = day;
 
     const icon = new Image();
     icon.src = logoSrc;
@@ -30,18 +49,18 @@ const renderDaily = (daily) => {
 
     const temperature = document.createElement('h3');
     temperature.classList.add('temperature');
-    temperature.textContent = day.temp.day;
+    temperature.textContent = daily[i].temp.day;
 
     const forecast = document.createElement('p');
     forecast.classList.add('forecast');
-    forecast.textContent = day.weather[0].main;
+    forecast.textContent = daily[i].weather[0].main;
 
     dailyWeather.appendChild(card);
     card.appendChild(weekday);
     card.appendChild(icon);
     card.appendChild(temperature);
     card.appendChild(forecast);
-  });
+  }
 };
 
 const renderHourly = (hourly) => {
