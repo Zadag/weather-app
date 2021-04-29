1f515f850a31412071747b18cc5ec436e55a9d72
Kevin <kbrowne97@gmail.com>
2021-04-29 11:12:57 -0400

    sets up template for daily weather cards

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,6 +1,8 @@
 import logoSrc from '../imgs/sun.png';
 
 const header = document.querySelector('header');
+const dailyWeather = document.querySelector('#daily-weather');
+const hourlyWeather = document.querySelector('#hourly-weather');
 
 const renderLogo = () => {
   const img = new Image();
@@ -11,6 +13,35 @@ const renderLogo = () => {
 
 const renderDaily = (daily) => {
   console.log(daily);
+  const date = new Date();
+  const today = date.getDay();
+
+  daily.forEach((day) => {
+    const card = document.createElement('div');
+    card.classList.add('daily-card');
+
+    const weekday = document.createElement('p');
+    weekday.classList.add('weekday');
+    weekday.textContent = 'Monday';
+
+    const icon = new Image();
+    icon.src = logoSrc;
+    icon.classList.add('weather-icon');
+
+    const temperature = document.createElement('h3');
+    temperature.classList.add('temperature');
+    temperature.textContent = day.temp.day;
+
+    const forecast = document.createElement('p');
+    forecast.classList.add('forecast');
+    forecast.textContent = day.weather[0].main;
+
+    dailyWeather.appendChild(card);
+    card.appendChild(weekday);
+    card.appendChild(icon);
+    card.appendChild(temperature);
+    card.appendChild(forecast);
+  });
 };
 
 const renderHourly = (hourly) => {
